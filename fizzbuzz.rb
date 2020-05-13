e6b4e8684d8b7b0bd9978081782f5b70a202726c
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 01:22:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 && int % 5 == 0
+  "FizzBuzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  elsif int % 5 == 0
+  else int % 5 == 0
     "Buzz"
-  else
-    int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
+
   end
 end
