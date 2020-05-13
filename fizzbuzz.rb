f0c16c06f76a36e915803cead4e76bf4ab7f75d2
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 01:18:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,5 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
-#puts "Give me a number and let's check"
-#int = gets.strip
-#def fizzbuzz(number)
-
-#end
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  end
+end
