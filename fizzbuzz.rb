f77771d4b1d2c4a260c1da190a3d8608a1d7dd9b
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 00:36:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,15 +3,7 @@
 #puts "Give me a number and let's check"
 #int = gets.strip
 def fizzbuzz(number)
-  if number %3 == 0 && number % 5 == 0
-     "FizzBuzz"
-  elsif number%5 == 0
-     "Buzz"
-  elsif number%3 == 0
-    "Fizz"
-  else
-    puts nil
-  end
+  
 end
 
 fizzbuzz(number)
